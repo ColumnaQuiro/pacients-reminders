47d1d64aea43b5bf251a77943b58c99fb9f677e1
rcluis <raulcatalanluis@gmail.com>
2024-08-21 20:57:31 +0200

    added: netlify functions

--- netlify/functions/patients-reminders/patients-reminders.mjs
+++ netlify/functions/patients-reminders/patients-reminders.mjs
@@ -1,7 +1,7 @@
 import { schedule } from '@netlify/functions'
 import { sendPatientsReminders } from '../../../src/main.js'
 
-export const handler = schedule('9 0 * * *', async (event) => {
+export const handler = schedule('0 9 * * *', async (event) => {
   await sendPatientsReminders()
   return {
     statusCode: 200
