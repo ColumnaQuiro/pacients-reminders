dcd85ac0cb7c7fbdecfefc695cb83a618453bfda
rcluis <raulcatalanluis@gmail.com>
2024-08-21 20:43:20 +0200

    added: netlify functions

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
 export const PRACTICE_HUB_API = 'https://columnaquiropracticehub.neptune.practicehub.io/api'
 export const WHATSAPP_API_MESSAGES = 'https://graph.facebook.com/v20.0/387933511072949/messages'
-export const SLACK_CHANNEL = 'https://hooks.slack.com/services/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'
+export const SLACK_CHANNEL = 'https://hooks.slack.com/services/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'
